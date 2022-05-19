49e4958d37d72a62d95968262f39915900050174
Den <d3n.vor@gmail.com>
2022-05-19 15:18:51 +0300

    added filter and search.

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -1,8 +1,30 @@
 import "./app-filter.css";
 
-const AppFilter = () => {
+const AppFilter = (props) => {
+
+    const buttonsData = [
+        {name: 'all', label: 'All Employees'},
+        {name: 'rise', label: 'On rise'},
+        {name: 'moreThan1000', label: 'Salary > 1000$'}
+    ];
+
+    const buttons = buttonsData.map(({name, label}) => {
+        const active = props.filter === name;
+        const clazz = active ? 'btn-light' : 'btn-outline-light';
+        return (
+            <button 
+                className={`btn ${clazz}`}
+                type="button"
+                key={name}
+                onClick={() => props.onFilterSelect(name)}>
+                    {label}
+            </button>
+        )
+    })
     return (
         <div className="btn-group">
+            {buttons}
+            {/* 
             <button 
                 className="btn btn-light"
                 type="button">
@@ -18,7 +40,7 @@ const AppFilter = () => {
                 type="button">
                     З/П больше 1000$
             </button>
-            
+             */}
         </div>
     )
 }
