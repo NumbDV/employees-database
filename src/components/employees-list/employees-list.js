20f085e807881d8400affa33ec013828fd5433ad
Den <d3n.vor@gmail.com>
2022-05-19 12:28:54 +0300

    added ability to add/delete employees, counter and benefits

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,12 +2,17 @@ import EmployeesListItem from '../employees-list-item/employees-list-item';
 
 import './employees-list.css';
 
-const EmployeesList = ({data}) => {
+const EmployeesList = ({data, onDelete, onToggleProp, /* onToggleIncrease, onToggleRise */}) => {
 
     const elements = data.map(item => {
         const {id, ...itemProps} = item;
         return (
-            <EmployeesListItem key={id} {...itemProps}/> //name={item.name} salary={item.salary}
+            <EmployeesListItem 
+                key={id} {...itemProps} //name={item.name} salary={item.salary}
+                onDelete={() => onDelete(id)}
+                // onToggleIncrease={() => onToggleIncrease(id)}
+                // onToggleRise={() => onToggleRise(id)}
+                onToggleProp={(e) => onToggleProp(id, e.currentTarget.getAttribute('data-toggle'))}/> 
         )
     })
     return (
